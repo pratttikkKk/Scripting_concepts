9c4bf93bc9fb6972a5978c3ac6b450f2334995d2
Pratik Suresh farate <pratikfarate33@gmail.com>
2026-05-21 11:46:41 +0530

    first commit

---
+++ Dart/student.dart
@@ -0,0 +1,13 @@
+class student{
+  void Checkattendance(){
+   double attendance = 80;
+    if(attendance >=75 ){
+      print("'your are eligible")
+    }
+  }
+}
+void main(){
+  student s = student();
+  s.Checkattendance();
+
+}
